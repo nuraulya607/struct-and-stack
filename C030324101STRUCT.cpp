77d8e6d69472cf2d5d78ce500aff6fa19e315c0e
nurauliawati <nuraulya607@gmail.com>
2025-06-10 01:20:02 +0800

    penggunaan struct berbasis pointer

--- C030324101STRUCT.cpp
+++ C030324101STRUCT.cpp
@@ -10,17 +10,20 @@ struct mahasiswa {
 };
 
 int main() {
-    mahasiswa aulia;
+    mahasiswa *auliaPtr = new mahasiswa; 
 
-    aulia.nim = "c030324101";
-    aulia.nama = "nur aulia wati";
-    aulia.alamat = "Handil Bakti, Komp Persada Permai Baru 1, no 191";
-    aulia.ipk = 3.8;
+    auliaPtr->nim = "c030324101";
+    auliaPtr->nama = "nur aulia wati";
+    auliaPtr->alamat = "Handil Bakti, Komp Persada Permai Baru 1, no 191";
+    auliaPtr->ipk = 3.8;
 
-    cout << "NIM    : " << aulia.nim << "\n";
-    cout << "Nama   : " << aulia.nama << "\n";
-    cout << "Alamat : " << aulia.alamat << "\n";
-    cout << "IPK    : " << aulia.ipk << "\n";
+    cout << "NIM    : " << auliaPtr->nim << "\n";
+    cout << "Nama   : " << auliaPtr->nama << "\n";
+    cout << "Alamat : " << auliaPtr->alamat << "\n";
+    cout << "IPK    : " << auliaPtr->ipk << "\n";
+
+    delete auliaPtr;
+    auliaPtr = nullptr;
 
     return 0;
 }
